9cd4c6cd0209b2bf0bc51d7de110ee44dd9fd0d6
unknown <shayshn1234@gmail.com>
2025-10-26 17:09:24 +0200

    adedd

--- return.py
+++ return.py
@@ -1,8 +1,8 @@
 from operator import le
 import random
 
-suites = ["H","C","D","S"]
-ranks = {"2": 2,
+optinal_suites = ["H","C","D","S"]
+optinal_ranks = {"2": 2,
              "3": 3,
              "4": 4,
              "5": 5,
@@ -16,11 +16,11 @@ ranks = {"2": 2,
              "K": 13,
              "A": 14,}
 def create_card(rank:str,suite:str) -> dict:
-    if rank not in ranks or suite not in suites:
+    if rank not in optinal_ranks or suite not in optinal_suites:
         return None 
     return {"rank": rank,
             "suite": suite,
-            "value": ranks[rank]}
+            "value": optinal_ranks[rank]}
 
 def compare_cards(p1_card:dict, p2_card:dict) -> str:
     if p1_card["value"] > p2_card["value"]:
@@ -31,9 +31,9 @@ def compare_cards(p1_card:dict, p2_card:dict) -> str:
         return 'WAR'
     
 def create_deck() -> list[dict]:
-    arr = []
-    for i in suites:
-        for j in ranks:
-            arr.append(create_card(j,i))
-    return arr
+    result = []
+    for suite in optinal_suites:
+        for rank in optinal_ranks:
+            result.append(create_card(rank,suite))
+    return result
         
