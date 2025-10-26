19b910250a8d1c96d6fb9cc9f7d87a7af8fcf73e
unknown <shayshn1234@gmail.com>
2025-10-26 15:56:12 +0200

    shit

--- return.py
+++ return.py
@@ -1,7 +1,23 @@
 import random
 
 def create_card(rank:str,suite:str) -> dict:
-    return dict(rank = str(rank), suite = suite, value = rank)
+    ranks = {"2": 2,
+             "3": 3,
+             "4": 4,
+             "5": 5,
+             "6": 6,
+             "7": 7,
+             "8": 8,
+             "9": 9,
+             "10": 10,
+             "J": 11,
+             "Q": 12,
+             "K": 13,
+             "A": 14,}
+    
+    return {"rank": rank,
+            "suite": suite,
+            "value": ranks[rank]}
 
 def compare_cards(p1_card:dict, p2_card:dict) -> str:
     if p1_card["value"] > p2_card["value"]:
